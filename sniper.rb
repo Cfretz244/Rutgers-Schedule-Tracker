76216434e87ae2e49fecbd94e3d3a34f40778b61
root <root@christopherfretz.com>
2014-04-10 14:26:19 +0000

    initial ruby commit

---
+++ sniper.rb
@@ -0,0 +1,34 @@
+require "net/http"
+require "uri"
+require "json"
+require "debugger"; debugger
+require "twilio-ruby"
+class Course
+    attr_accessor :dep, :course, :sec
+    def initialize department, course, section = nil
+        @dep = department
+        @course = course
+        @sec = section
+    end
+end
+
+
+courses = [Course.new(447, 380, 20), Course.new(447, 414, 1)]
+for i in 0..courses.length - 1
+    chosenCourse = courses[i]
+    uri = URI.parse "http://sis.rutgers.edu/soc/courses.json?subject=#{chosenCourse.dep}&semester=92014&campus=NB&level=U"
+    rawResponse = Net::HTTP.get_response uri
+    response = JSON.parse rawResponse.body
+    for course in response
+        if course["courseNumber"].to_i == chosenCourse.course
+            sections = course["sections"]
+            for section in sections
+                if chosenCourse.sec && section['number'].to_i == chosenCourse.sec && section['openStatus']
+                    puts "Hot diggity! #{course['title']} section #{section['number']} is open! Go! Go! Go!"
+                elsif !chosenCourse.sec && section['openStatus']
+                    puts "Hot diggity! #{course['title']} is open! Go! Go! Go!"
+                end
+            end
+        end
+    end
+end

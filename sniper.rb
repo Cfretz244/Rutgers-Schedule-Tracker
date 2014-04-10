563785352cf9c95186417e7df00d08918b34bd54
root <root@christopherfretz.com>
2014-04-10 15:12:09 +0000

    basic functionality complete

--- sniper.rb
+++ sniper.rb
@@ -10,24 +10,41 @@ class Course
         @course = course
         @sec = section
     end
+
+    def == another_course
+        return false if !another_course.is_a?(self.class)
+        @dep == another_course.dep && @course == another_course.course && @sec = another_course.sec
+    end
 end
 
 account_sid = "AC3c4e47084e170e028847ee3dbfef6cd0"
+auth_token = "10c79fad2ebb7911cb8c1f9c2a5f1ad8"
+client = Twilio::REST::Client.new account_sid, auth_token
 
-courses = [Course.new(447, 380, 20), Course.new(447, 414, 1)]
-for i in 0..courses.length - 1
-    chosenCourse = courses[i]
-    uri = URI.parse "http://sis.rutgers.edu/soc/courses.json?subject=#{chosenCourse.dep}&semester=92014&campus=NB&level=U"
-    rawResponse = Net::HTTP.get_response uri
-    response = JSON.parse rawResponse.body
-    for course in response
-        if course["courseNumber"].to_i == chosenCourse.course
-            sections = course["sections"]
-            for section in sections
-                if chosenCourse.sec && section['number'].to_i == chosenCourse.sec && section['openStatus']
-                    puts "Hot diggity! #{course['title']} section #{section['number']} is open! Go! Go! Go!"
-                elsif !chosenCourse.sec && section['openStatus']
-                    puts "Hot diggity! #{course['title']} is open! Go! Go! Go!"
+miriamCourses = [Course.new(447, 380, 20)]
+chrisCourses = []
+for k in 0..1
+    k == 0 ? courses = miriamCourses : courses = chrisCourses
+    for i in 0..courses.length - 1
+        chosenCourse = courses[i]
+        uri = URI.parse "http://sis.rutgers.edu/soc/courses.json?subject=#{chosenCourse.dep}&semester=92014&campus=NB&level=U"
+        rawResponse = Net::HTTP.get_response uri
+        response = JSON.parse rawResponse.body
+        for course in response
+            if course["courseNumber"].to_i == chosenCourse.course
+                sections = course["sections"]
+                for section in sections
+                    if chosenCourse.sec && section['number'].to_i == chosenCourse.sec && section['openStatus']
+                        message = "Hot diggity! #{course['title']} section #{section['number']} is open! Go! Go! Go!"
+                        miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
+                        puts to
+                        #client.account.sms.messages.create(:body => message, :to => to, :from => "2674332999")
+                    elsif !chosenCourse.sec && section['openStatus']
+                        message = "Hot diggity! #{course['title']} is open! Go! Go! Go!"
+                        miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
+                        puts to
+                        #client.account.sms.messages.create(:body => message, :to => to, :from => "2674332999")
+                    end
                 end
             end
         end

0f9390b82a50fb1fad028de40512c6a726470247
root <root@christopherfretz.com>
2014-04-10 15:39:22 +0000

    routine commit

--- sniper.rb
+++ sniper.rb
@@ -1,7 +1,6 @@
 require "net/http"
 require "uri"
 require "json"
-require "debugger"; debugger
 require "twilio-ruby"
 class Course
     attr_accessor :dep, :course, :sec
@@ -22,31 +21,32 @@ auth_token = "10c79fad2ebb7911cb8c1f9c2a5f1ad8"
 client = Twilio::REST::Client.new account_sid, auth_token
 
 miriamCourses = [Course.new(447, 380, 20)]
-chrisCourses = []
-for k in 0..1
-    k == 0 ? courses = miriamCourses : courses = chrisCourses
-    for i in 0..courses.length - 1
-        chosenCourse = courses[i]
-        uri = URI.parse "http://sis.rutgers.edu/soc/courses.json?subject=#{chosenCourse.dep}&semester=92014&campus=NB&level=U"
-        rawResponse = Net::HTTP.get_response uri
-        response = JSON.parse rawResponse.body
-        for course in response
-            if course["courseNumber"].to_i == chosenCourse.course
-                sections = course["sections"]
-                for section in sections
-                    if chosenCourse.sec && section['number'].to_i == chosenCourse.sec && section['openStatus']
-                        message = "Hot diggity! #{course['title']} section #{section['number']} is open! Go! Go! Go!"
-                        miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
-                        puts to
-                        #client.account.sms.messages.create(:body => message, :to => to, :from => "2674332999")
-                    elsif !chosenCourse.sec && section['openStatus']
-                        message = "Hot diggity! #{course['title']} is open! Go! Go! Go!"
-                        miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
-                        puts to
-                        #client.account.sms.messages.create(:body => message, :to => to, :from => "2674332999")
+chrisCourses = [Course.new(198, 105, 1), Course.new(198, 417, 1)]
+for count in 0..19
+    for k in 0..1
+        k == 0 ? courses = miriamCourses : courses = chrisCourses
+        for i in 0..courses.length - 1
+            chosenCourse = courses[i]
+            uri = URI.parse "http://sis.rutgers.edu/soc/courses.json?subject=#{chosenCourse.dep}&semester=92014&campus=NB&level=U"
+            rawResponse = Net::HTTP.get_response uri
+            response = JSON.parse rawResponse.body
+            for course in response
+                if course["courseNumber"].to_i == chosenCourse.course
+                    sections = course["sections"]
+                    for section in sections
+                        if chosenCourse.sec && section['number'].to_i == chosenCourse.sec && section['openStatus']
+                            message = "Hot diggity! #{course['title']} section #{section['number']} is open! Go! Go! Go!"
+                            miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
+                            client.account.sms.messages.create(:body => message, :to => to, :from => "2674332999")
+                        elsif !chosenCourse.sec && section['openStatus']
+                            message = "Hot diggity! #{course['title']} is open! Go! Go! Go!"
+                            miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
+                            client.account.sms.messages.create(:body => message, :to => to, :from => "2674332999")
+                        end
                     end
                 end
             end
         end
     end
+    sleep 30
 end

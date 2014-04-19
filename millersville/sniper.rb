e0f6f20aaea841559185c64bd65861ee155f7006
root <root@christopherfretz.com>
2014-04-19 17:25:09 +0000

    changed a few things for millersville

--- millersville/sniper.rb
+++ millersville/sniper.rb
@@ -1,19 +1,21 @@
 require 'net/http'
+require 'set'
 require 'uri'
 require 'json'
 require 'nokogiri'
 require 'twilio-ruby'
 class Course
-    attr_accessor :dep, :course, :sec
-    def initialize department, course, section = nil
+    attr_accessor :dep, :specDep, :course, :sec
+    def initialize department, specificDepartment, course, section = nil
         @dep = department
+        @specDep = specificDepartment
         @course = course
         @sec = section
     end
 
     def == another_course
         return false if !another_course.is_a?(self.class)
-        @dep == another_course.dep && @course == another_course.course && @sec = another_course.sec
+        @dep == another_course.dep and @course == another_course.course and @sec = another_course.sec and @specDep == another_course.specDep
     end
 end
 
@@ -27,7 +29,7 @@ account_sid = "AC3c4e47084e170e028847ee3dbfef6cd0"
 auth_token = "10c79fad2ebb7911cb8c1f9c2a5f1ad8"
 client = Twilio::REST::Client.new account_sid, auth_token
 
-heatherCourses = [Course.new('GOVT', 111, 0), Course.new('GOVT', 111, 1)]
+heatherCourses = [Course.new('ART', 'ART', 203, 1), Course.new('GOVT', 'GOVT', 112), Course.new('GOVT', 'GOVT', 221), Course.new('COMM', 'COMM', 201)]
 begin
     while true
         for course in heatherCourses
@@ -52,7 +54,7 @@ begin
                         if (child.children.size > 0 or child.text != "\n") and not child['colspan']
                             stripped = child.text.strip.delete "\n"
                             if stripped and stripped != ''
-                                if column == 1 and stripped.include?(course.dep) and stripped.include?(course.course.to_s)
+                                if column == 1 and stripped.include?(course.specDep) and stripped.include?(course.course.to_s)
                                     if course.sec and stripped.split(' ')[2].to_i == course.sec
                                         target = true
                                     elsif not course.sec
@@ -63,7 +65,7 @@ begin
                                     name = stripped
                                 end
                                 if column == 9 and stripped.is_integer? and stripped.to_i > 0 and target
-                                    course.sec ? message = "#{name}, section ##{course.sec} is open!" : message = "#{name} is open!"
+                                    course.sec ? message = "Hot diggity! #{name}, section ##{course.sec} is open! Go! Go! Go!" : message = "Hot diggity! #{name} is open! Go! Go! Go!"
                                     client.account.sms.messages.create(:body => message, :to => '6108125436', :from => '2674332999')
                                 end
                                 column += 1

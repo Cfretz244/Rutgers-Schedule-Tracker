7d7d7a08147e028bd57111eacb614f7cd89a1251
Chris Fretz <Cfretz@icloud.com>
2014-11-19 22:57:22 +0000

    added simple exponential backoff

--- rutgers/sniper.rb
+++ rutgers/sniper.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require "net/http"
 require "uri"
 require "json"
@@ -18,17 +19,18 @@ end
 
 account_sid = "AC3c4e47084e170e028847ee3dbfef6cd0"
 auth_token = "10c79fad2ebb7911cb8c1f9c2a5f1ad8"
+e_count = 0
 client = Twilio::REST::Client.new account_sid, auth_token
 
-miriamCourses = [Course.new(447, 380, 20)]
-chrisCourses = [Course.new(198, 344), Course.new(198, 214, 1), Course.new(198, 214, 2), Course.new(198, 214, 3), Course.new(198, 214, 4)]
+chrisCourses = [Course.new(198, 213, 3), Course.new(198, 352, 1)]
+miriamCourses = []
 begin
     while true
         for k in 0..1
             k == 0 ? courses = miriamCourses : courses = chrisCourses
             for i in 0..courses.length - 1
                 chosenCourse = courses[i]
-                uri = URI.parse "http://sis.rutgers.edu/soc/courses.json?subject=#{chosenCourse.dep}&semester=92014&campus=NB&level=U"
+                uri = URI.parse "https://sis.rutgers.edu/soc/courses.json?subject=#{chosenCourse.dep}&semester=12015&campus=NB&level=U"
                 rawResponse = Net::HTTP.get_response uri
                 response = JSON.parse rawResponse.body
                 for course in response
@@ -39,10 +41,12 @@ begin
                                 message = "Hot diggity! #{course['title']} section #{section['number']} is open! To allow the fastest registration possible, the index number of the course is #{section['index']}. Go! Go! Go!"
                                 miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
                                 client.account.sms.messages.create(:body => message, :to => to, :from => '2674332999')
+                                e_count = 0
                             elsif !chosenCourse.sec && section['openStatus']
                                 message = "Hot diggity! #{course['title']} section #{section['number']} is open! To allow the fastest registration possible, the index number of the course is #{section['index']}. Go! Go! Go!"
                                 miriamCourses.include?(chosenCourse) ? to = '8138920100' : to = '2152370055'
                                 client.account.sms.messages.create(:body => message, :to => to, :from => '2674332999')
+                                e_count = 0
                             end
                         end
                     end
@@ -51,6 +55,13 @@ begin
         end
         sleep 30
     end
-rescue
-    client.account.sms.messages.create(:body => 'Script has crashed', :to => '2152370055', :from => '2674332999')
+rescue => e
+    if e_count < 5
+        sleep(e_count**2)
+        e_count += 1
+        retry
+    else
+        File.open('exception.txt', 'wb') {|file| f << "Exception: #{e.class}\nMessage: #{e.message}" }
+        exit
+    end
 end
